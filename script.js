a471e1f4ced813f2e77256a5aa7e3719f99a2db5
andreamaluta <andreamaluta@gmail.com>
2025-04-14 16:19:53 +0200

    [UPD] finish the exercise

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const createName = require("./names.js");
 const createHobbies = require("./hobbies.js");
 
+function all (){
+    const name = createName('andrea', 'maluta');
+    const hobbie = createHobbies('film', 'film', 'film');
+    return{
+        name,
+        hobbie
+    }
+}
 
+const allitem = all();
+
+console.log(allitem);
 
 
 // const fullName = createName.name(andrea, maluta);
