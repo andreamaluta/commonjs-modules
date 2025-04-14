97659a696ffda03b92a9671f8c756ed56aa70284
andreamaluta <andreamaluta@gmail.com>
2025-04-14 16:03:19 +0200

    [UPD] made the export

---
+++ hobbies.js
@@ -0,0 +1,9 @@
+function hobbies (hobbyOne, hobbyTwo, hobbyThree){
+    const hobbie = [];
+    hobbie.push(hobbyOne);
+    hobbie.push(hobbyTwo);
+    hobbie.push(hobbyThree);
+    return hobbie;
+}
+
+module.exports = hobbies;
